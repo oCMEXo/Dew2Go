2b61f09b64c0dbb377a46e8c9fcf3889b3ec17e3
artem <artsiomharminovich05@gmail.com>
2025-10-16 14:38:28 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Dew2Go/', // 👈 добавь это (имя репозитория, с учётом регистра!)
 })
